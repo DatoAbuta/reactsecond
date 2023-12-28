ffa1213f020d25039fbddf915ea0caddebb3f9f1
David69699 <abutidzedato18@gmail.com>
2023-12-28 18:17:40 +0400

    update

--- src/Cardebi.js
+++ src/Cardebi.js
@@ -1,6 +1,6 @@
 export const data = [
     {
-       imgUrl: './public/pipia1.svg',
+       imgUrl: './pipia1.svg',
        name: 'Sedans',
        desc: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
        button: 'Learn More',
@@ -12,7 +12,7 @@ export const data = [
        name: 'SUVS',
        desc: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
        button: 'Learn More',
-       imgUrl: './public/pipia2.svg',
+       imgUrl: './pipia2.svg',
        hover: 'aqua',
        backgroundColor: '#006971',
        buttonColor: '#006971'
@@ -21,7 +21,7 @@ export const data = [
        name: 'Luxury',
        desc: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style. ',
        button: 'Learn More',
-       imgUrl: './public/pipia3.svg',
+       imgUrl: './pipia3.svg',
        hover: 'blue',
        backgroundColor: '#004140',
        buttonColor: '#006971'
